22ed686efd88f13bc51b391e4e12cb52b7a7d7d6
Derek Hamel <dbhamel@asu.edu>
2015-11-14 11:37:35 -0700

    initial

---
+++ main.js
@@ -0,0 +1,72 @@
+var path = "/";	// STUDENT: set to "" to run on your computer, "/" for submitting code to Udacity
+
+var camera, scene, renderer;
+var cameraControls;
+var clock = new THREE.Clock();
+
+function fillScene() {
+    scene = new THREE.Scene();
+
+    var myPolygon = new SquareGeometry();
+    var texture = THREE.ImageUtils.loadTexture('phx.jpg');
+    texture.wrapS = THREE.ClampToEdgeWrapping;
+    texture.wrapT = THREE.ClampToEdgeWrapping;
+    var myPolygonMaterial = new THREE.MeshBasicMaterial( { map: texture } );
+    var polygonObject = new THREE.Mesh( myPolygon, myPolygonMaterial );
+    scene.add(polygonObject);
+}
+
+function SquareGeometry() {
+    var geo = new THREE.PlaneGeometry(100,100);
+
+    return geo;
+}
+
+function addToDOM() {
+    var container = document.getElementById('container');
+    var canvas = container.getElementsByTagName('canvas');
+    if (canvas.length>0) {
+        container.removeChild(canvas[0]);
+    }
+    container.appendChild( renderer.domElement );
+}
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    render();
+}
+function render() {
+    var delta = clock.getDelta();
+    cameraControls.update(delta);
+    renderer.render(scene, camera);
+}
+
+function init() {
+    var canvasWidth = window.innerWidth-20;
+    var canvasHeight = window.innerHeight-20;
+
+    var canvasRatio = canvasWidth / canvasHeight;
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+    renderer.setSize(canvasWidth, canvasHeight);
+
+    // Camera: Y up, X right, Z up
+    camera = new THREE.PerspectiveCamera(30, canvasWidth / canvasHeight, 1, 10000);
+    camera.position.set(0, 0, 100);
+
+    // CONTROLS
+    cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
+    cameraControls.target.set(0, 0, 0);
+}
+
+try {
+    init();
+    fillScene();
+    addToDOM();
+    animate();
+} catch (e) {
+    var errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
+    $('#container').append(errorReport + e);
+}
---
+++ server.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+app.get('/:name', function (req, res, next) {
+
+  var options = {
+    root: __dirname + '/',
+    dotfiles: 'allow',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+
+});
+
+app.get('/lib/:name', function (req, res, next) {
+
+  var options = {
+    root: __dirname + '/lib/',
+    dotfiles: 'allow',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
