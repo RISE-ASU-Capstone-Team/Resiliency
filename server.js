0534c279a4c4b4d08ab5ede86c9b4be2adb63c16
Jacob Dobkins <jacob.dobkins@outlook.com>
2016-01-16 11:49:55 -0700

    Refactored code, and replaced THREE.JS w/ Leaflet

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var app = express();
-var path = require("path");
 
 var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Resiliency app listening at http://%s:%s', host, port);
 });
+
+//Default "/" address maps to index.html
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
+
+//Recieve files from / directory.
 app.get('/:name', function (req, res, next) {
 
   var options = {
@@ -35,6 +38,7 @@ app.get('/:name', function (req, res, next) {
 
 });
 
+//Recieve files from /lib/ directory.
 app.get('/lib/:name', function (req, res, next) {
 
   var options = {
@@ -59,17 +63,57 @@ app.get('/lib/:name', function (req, res, next) {
 
 });
 
+//Recieve files from /layout/ directory.
+app.get('/layout/:name', function (req, res, next) {
+
+  var options = {
+    root: __dirname + '/layout/',
+    dotfiles: 'allow',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+
+});
+
+//Recieve files from /css/ directory.
+app.get('/css/:name', function (req, res, next) {
+
+  var options = {
+    root: __dirname + '/css/',
+    dotfiles: 'allow',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+
+});
+
 // accept POST request on the homepage
 app.post('/', function (req, res) {
   res.send('Got a POST request');
-});
-
-// accept PUT request at /user
-app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user');
-});
-
-// accept DELETE request at /user
-app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user');
 });
