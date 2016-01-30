89ae1120dd4634671305258b1ee203a1fd53df07
gtikhono <gtikhono@asu.edu>
2016-01-29 17:49:12 -0700

    added the full js object with the project portion

--- src/infrastructureMenu.js
+++ src/infrastructureMenu.js
@@ -1,14 +1,15 @@
-var jsonObjectTest = {
-
-        name: "IEEE 123 Bus Feeder",
-        type: "SG: Unbalanced",
-        format: "ANSI standard",
-        transformerDelta: "Dyn 1",
-        units: "Imperial,(kV) kilovolts ,(kA) kiloamps, (kVA) kvolt-amp",
-        frequency: "  60 Hz",
-        temperature: "(F) Fahrenheit",
-        description: "The IEEE 123 node test",
-    
+var jsonObjectTest = 
+{
+    "project": {
+        "name": "IEEE 123 Bus Feeder",
+        "type": "SG: Unbalanced",
+        "format": "ANSI standard",
+        "transformerDelta": "Dyn 1",
+        "units": ["     Imperial","(kV) kilovolts","(kA) kiloamps","(kVA) kvolt-amp"],
+        "frequency": "  60 Hz",
+        "temperature": "(F) Fahrenheit",
+        "description": "The IEEE 123 node test",
+    }
 }
 
 
@@ -26,13 +27,13 @@ function jsontoTable(jsonObject){
     //   console.log("Error when reading from Local Storage\n" + e);        
     // }
 
-	document.getElementById("name").innerHTML=jsonObject.name;
-	document.getElementById("type").innerHTML=jsonObject.type;
-	document.getElementById("format").innerHTML=jsonObject.format;
-	document.getElementById("transformerDelta").innerHTML=jsonObject.transformerDelta;
-	document.getElementById("units").innerHTML=jsonObject.units;
-	document.getElementById("frequency").innerHTML=jsonObject.frequency;
-	document.getElementById("temperature").innerHTML=jsonObject.temperature;
-	document.getElementById("description").innerHTML=jsonObject.description;
+	document.getElementById("name").innerHTML=jsonObject.project.name;
+	document.getElementById("type").innerHTML=jsonObject.project.type;
+	document.getElementById("format").innerHTML=jsonObject.project.format;
+	document.getElementById("transformerDelta").innerHTML=jsonObject.project.transformerDelta;
+	document.getElementById("units").innerHTML=jsonObject.project.units;
+	document.getElementById("frequency").innerHTML=jsonObject.project.frequency;
+	document.getElementById("temperature").innerHTML=jsonObject.project.temperature;
+	document.getElementById("description").innerHTML=jsonObject.project.description;
 	
 }
