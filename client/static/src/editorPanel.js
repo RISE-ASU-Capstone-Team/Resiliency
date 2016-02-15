f3b6bd015a0d74dc260f56cae95aea6704f8d825
Graydon Svendson <gsvendso@asu.edu>
2016-02-15 12:30:29 -0700

    Fixed Editor Panel Icon Issues

--- client/static/src/editorPanel.js
+++ client/static/src/editorPanel.js
@@ -1,95 +1,10 @@
 
-
- $.ajax({
-     url: "/static/components.json",
-     success: function (data) {
-         var obj = JSON.parse(data);
-         loadPanelObjects(obj);
-     }
- });
-
- var testData = "{ \
-   \"Components\" : [ \
-     { \
-       \"System\": \"Power\", \
-       \"Subsystem\": \"Generation Subsystem\", \
-       \"RiseVariableName\": \"genSynchronous\", \
-       \"Description\": \"Synchronous Generator\", \
-       \"OpenDssName\": \"null\", \
-       \"Icon\": \"data/MultiCell-Icon.png\", \
-       \"Variables\": [ \
-         { \
-           \"VariableName\": \"status\", \
-           \"OpenDssVariable\": \"\", \
-           \"Units\": \"none\", \
-           \"Type\": \"boolean\", \
-           \"OpenDssType\": \"\", \
-           \"ValueRange\": \"on/off\", \
-           \"Description\": \"Status\", \
-           \"OpenDssV_Name\": \"\", \
-           \"Comments\": \"\" \
-         }, \
-         { \
-           \"VariableName\": \"connectBusNumber\", \
-           \"OpenDssVariable\": \"\", \
-           \"Units\": \"none\", \
-           \"Type\": \"integer\", \
-           \"OpenDssType\": \"\", \
-           \"ValueRange\": \"positive\", \
-           \"Description\": \"Bus number (connected)\", \
-           \"OpenDssV_Name\": \"\", \
-           \"Comments\": \"\" \
-         } \
-       ] \
-     }, \
-     { \
-       \"System\": \"Power\", \
-       \"Subsystem\": \"Generation Subsystem\", \
-       \"RiseVariableName\": \"genSolarPVMod\", \
-       \"Description\": \"Solar PV Module\", \
-       \"OpenDssName\": \"null\", \
-       \"Icon\": \"data/FixedResister-Icon.png\", \
-       \"Variables\": [ \
-         { \
-           \"VariableName\": \"capacity\", \
-           \"OpenDssVariable\": \"\", \
-           \"Units\": \"kW\", \
-           \"Type\": \"double\", \
-           \"OpenDssType\": \"\", \
-           \"ValueRange\": \"zero or positive\", \
-           \"Description\": \"Capacity\", \
-           \"OpenDssV_Name\": \"\", \
-           \"Comments\": \"Rated DC output, also known as 'maximum power'\" \
-         }, \
-         { \
-           \"VariableName\": \"azimuth\", \
-           \"OpenDssVariable\": \"\", \
-           \"Units\": \"degrees\", \
-           \"Type\": \"double\", \
-           \"OpenDssType\": \"\", \
-           \"ValueRange\": \"-180 to 180\", \
-           \"Description\": \"Azimuth\", \
-           \"OpenDssV_Name\": \"\", \
-           \"Comments\": \"0 degrees is due South, positive degrees rotation clockwise\" \
-         } \
-       ] \
-     } \
-   ] \
- }";
-
 var componentsArr = null;
-
-initEditorPanel();
-
- function initEditorPanel() {
-   //var testObj = JSON.parse(testData);
+function initEditorPanel() {
    componentsArr = new Array();
-
-   //loadPanelObjects(testObj);
 }
 
 function loadPanelObjects(jsonobj) {
-  //console.log(jsonobj);
 
   for (var i = 0; i < jsonobj.Components.length; i++) {
     var component = jsonobj.Components[i]
@@ -119,6 +34,13 @@ function loadPanelObjects(jsonobj) {
 var dragMousePositionX, dragMousePositionY;
 
 $(document).ready(function() {
+
+  initEditorPanel();
+
+  $.getJSON( "/static/components.json" , function( result ){
+      loadPanelObjects(result);
+  });
+
   $("#editor .tab-pane")
     .bind("dragstart", function(ev) {
       if (!$(ev.target).hasClass("dragComponent")) return true;
